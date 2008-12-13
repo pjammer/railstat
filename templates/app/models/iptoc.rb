e9e7c90f885fa78deda4e27ad6b73306eb23f886
nerbie69 <philipjingram@yahoo.com>
2008-12-12 22:19:29 -0500

    deleted:    ../../rail_stat_generator.rb 	deleted:    ../../templates/README 	deleted:    ../../templates/app/controllers/rail_stat_controller.rb 	deleted:    ../../templates/app/helpers/rail_stat_helper.rb 	deleted:    ../../templates/app/models/iptoc.rb 	deleted:    ../../templates/app/models/rail_stat.rb 	deleted:    ../../templates/app/models/search_term.rb 	deleted:    ../../templates/app/views/layouts/rail_stat.rhtml 	deleted:    ../../templates/app/views/rail_stat/_menu.rhtml 	deleted:    ../../templates/app/views/rail_stat/hits.rhtml 	deleted:    ../../templates/app/views/rail_stat/lang.rhtml 	deleted:    ../../templates/app/views/rail_stat/other.rhtml 	deleted:    ../../templates/app/views/rail_stat/path.rhtml 	deleted:    ../../templates/app/views/rail_stat/platform.rhtml 	deleted:    ../../templates/app/views/rail_stat/refs.rhtml 	deleted:    ../../templates/db/ip-to-country.mysql.sql 	deleted:    ../../templates/db/railstat.rb 	deleted:    ../../templates/lib/path_tracker.rb 	deleted:    ../../templates/public/images/railstat/1.gif 	deleted:    ../../templates/public/images/railstat/10.gif 	deleted:    ../../templates/public/images/railstat/11.gif 	deleted:    ../../templates/public/images/railstat/12.gif 	deleted:    ../../templates/public/images/railstat/13.gif 	deleted:    ../../templates/public/images/railstat/14.gif 	deleted:    ../../templates/public/images/railstat/15.gif 	deleted:    ../../templates/public/images/railstat/16.gif 	deleted:    ../../templates/public/images/railstat/17.gif 	deleted:    ../../templates/public/images/railstat/18.gif 	deleted:    ../../templates/public/images/railstat/19.gif 	deleted:    ../../templates/public/images/railstat/1pxtr.gif 	deleted:    ../../templates/public/images/railstat/2.gif 	deleted:    ../../templates/public/images/railstat/20.gif 	deleted:    ../../templates/public/images/railstat/21.gif 	deleted:    ../../templates/public/images/railstat/22.gif 	deleted:    ../../templates/public/images/railstat/23.gif 	deleted:    ../../templates/public/images/railstat/24.gif 	deleted:    ../../templates/public/images/railstat/25.gif 	deleted:    ../../templates/public/images/railstat/26.gif 	deleted:    ../../templates/public/images/railstat/27.gif 	deleted:    ../../templates/public/images/railstat/28.gif 	deleted:    ../../templates/public/images/railstat/29.gif 	deleted:    ../../templates/public/images/railstat/3.gif 	deleted:    ../../templates/public/images/railstat/30.gif 	deleted:    ../../templates/public/images/railstat/31.gif 	deleted:    ../../templates/public/images/railstat/32.gif 	deleted:    ../../templates/public/images/railstat/33.gif 	deleted:    ../../templates/public/images/railstat/34.gif 	deleted:    ../../templates/public/images/railstat/35.gif 	deleted:    ../../templates/public/images/railstat/36.gif 	deleted:    ../../templates/public/images/railstat/37.gif 	deleted:    ../../templates/public/images/railstat/38.gif 	deleted:    ../../templates/public/images/railstat/39.gif 	deleted:    ../../templates/public/images/railstat/4.gif 	deleted:    ../../templates/public/images/railstat/40.gif 	deleted:    ../../templates/public/images/railstat/5.gif 	deleted:    ../../templates/public/images/railstat/6.gif 	deleted:    ../../templates/public/images/railstat/7.gif 	deleted:    ../../templates/public/images/railstat/8.gif 	deleted:    ../../templates/public/images/railstat/9.gif 	deleted:    ../../templates/public/images/railstat/other.gif 	deleted:    ../../templates/public/javascripts/railstat.js 	deleted:    ../../templates/public/stylesheets/railstat.css 	deleted:    ../../templates/public/stylesheets/tabs.css 	../../README

--- templates/app/models/iptoc.rb
+++
@@ -1,7 +0,0 @@
-require 'ipaddr'
-class Iptoc < ActiveRecord::Base
-  def self.find_by_ip_address(ip_address)
-    ip = IPAddr.new(ip_address)
-    find(:first, :conditions => ["IP_FROM <= ? AND IP_TO >= ?", ip.to_i, ip.to_i])
-  end
-end
